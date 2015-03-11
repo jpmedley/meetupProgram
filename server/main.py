4200de2b42373b1b6c843891b4212923e11ed835
jmedley@google.com <jmedley@google.com>
2015-03-11 15:05:14 -0700

    Implement rudimentary API handler.

--- server/main.py
+++ server/main.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
+import os
 from webapp2 import RequestHandler
 from webapp2 import WSGIApplication
 
-def MainHandler(RequestHandler):
+API_ORIGIN = "https://api.meetup.com"
+DASHBOARD = os.path.join(API_ORIGIN, "dashboard")
+
+class MainHandler(RequestHandler):
+  def get(self):
+    pass
+
+class Dashboard(RequestHandler):
   def get(self):
     pass
 
 app = WSGIApplication([
-  ('/'. MainHandler),
+  ('/', MainHandler),
+  ('/dashboard', Dashboard),
 ], debug=True)
